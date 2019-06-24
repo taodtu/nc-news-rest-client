fd150e5cdda3b95b8c3bd5a6b162e791d1be5d41
taodtu <taodtu@gmail.com>
2019-06-24 10:51:17 +0100

    install axios reach/router material-ui

---
+++ nc-news-rest-client/src/component/api.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+const request = axios.create({
+ baseURL: "https://nc-news-rest-api.herokuapp.com/api"
+})
+
+export const getTopics = () => {
+ return request.get('/topics').then(({ data }) => data.topics)
+};
+
+export const getUser = (username) => {
+ return request.get(`/users/${username}`).then(({ data }) => data.user)
+};
+
+export const getArticle = (article_id) => {
+ return request.get(`/articles/${article_id}`).then(({ data }) => data.article)
+};
+
+export const updateArticle = (id, body) => {
+ return request.patch(`/articles/${id}`, { ...body }).then(({ data }) => data.article)
+};
+
+export const addComment = (article_id, body) => {
+ return request.post(`/articles/${article_id}/comments`, { ...body }).then(({ data }) => data.comment)
+};
+
+export const getCommentsByArticle = (article_id) => {
+ return request.get(`/articles/${article_id}/comments`).then(({ data }) => data.comments)
+};
+
+export const addStudent = (student) => {
+ return request.post(`/students`, student).then(response => response)
+};
+
+export const deleteStudent = (id) => {
+ return request.delete(`/students/${id}`).then(response => response)
+};
+
+export const updateStudent = (id, progress) => {
+ return request.patch(`/students/${id}`, {}, { params: { progress: `${progress}` } }).then(response => response)
+};
