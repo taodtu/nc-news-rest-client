08f481354e41fbee7a1bea01e9d601d5f3f0f657
taodtu <taodtu@gmail.com>
2019-06-25 09:12:58 +0100

    adjust logo css

--- nc-news-rest-client/src/component/Header.js
+++ nc-news-rest-client/src/component/Header.js
@@ -4,7 +4,7 @@ import logo from './029-live.svg';
 const Header = () => {
  return (
   <header>
-   <img src={logo} alt="title" className="logo" />
+   <img src={logo} alt="title" className="logo" id="logo" />
    <h2>Nc News</h2>
   </header>
  );
