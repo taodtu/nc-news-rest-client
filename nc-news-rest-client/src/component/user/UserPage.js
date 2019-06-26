3975080e21231348b76b1a3d8f6b8143ba986c5c
taodtu <taodtu@gmail.com>
2019-06-26 15:08:53 +0100

    create the user page

---
+++ nc-news-rest-client/src/component/user/UserPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import UserItem from './UserItem';
+
+class UserPage extends Component {
+ render() {
+  return (
+   <div>
+
+   </div>
+  );
+ }
+}
+
+export default UserPage;
