a3be4eda53919f30a493b05af67336d56893741b
taodtu <taodtu@gmail.com>
2019-06-27 14:15:51 +0100

    rename Error component to ErrorMsg

--- nc-news-rest-client/src/component/error/Error.js
+++ nc-news-rest-client/src/component/error/Error.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Error = ({ error }) => {
- return (
-  <div>
-   {error.response.data.msg ? <p>{error.response.data.msg}</p>
-    : <p>Page not found</p>}
-  </div>
- );
+const ErrorMsg = ({ error }) => {
+  return (
+    <div>
+      {error.response.data.msg ? <p>{error.response.data.msg}</p>
+        : <p>Page not found</p>}
+    </div>
+  );
 };
 
-export default Error;
+export default ErrorMsg;
